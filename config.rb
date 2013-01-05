a49bcb7b7a3adcc3fab0dc0e8f91f4568aea0699
Jens Krause <info@websector.de>
2013-01-05 14:46:59 +0100

    config file for compass added

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+require 'susy'
+require 'animation'
+require 'respond-to'
+
+http_path = '/'
+
+sass_dir = 'src/styles'
+css_dir = 'src/jekyll/assets/stylesheets'
+http_stylesheets_path = http_path + 'assets/stylesheets'
+
+images_dir = 'src/jekyll/assets/images/'
+http_images_path = http_path + 'assets/images'
+
+fonts_dir = 'src/jekyll/assets/fonts/'
+http_fonts_dir = http_path + 'assets/fonts/'
+
+relative_assets = false
+preferred_syntax = :sass
+disable_warnings = true
