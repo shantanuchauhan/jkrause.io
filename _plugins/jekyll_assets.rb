e8120d51afff6b40cbf1a9cf8473f889c9fcbec6
Jens Krause <info@websector.de>
2013-01-11 19:35:23 +0100

    deactivating jekyll-assets, which may causes a bug - see: https://github.com/ixti/jekyll-assets/issues/3

--- _plugins/jekyll_assets.rb
+++ _plugins/jekyll_assets.rb
@@ -1,14 +1,16 @@
-require "jekyll-assets"
-require "jekyll-assets/compass"
-
-susy_dir = Gem::Specification.find_by_name("susy").gem_dir
-Sprockets.append_path File.join(susy_dir, "sass")
-
-respondto_dir = Gem::Specification.find_by_name("respond-to").gem_dir
-Sprockets.append_path File.join(respondto_dir, "stylesheets")
-
-breakpoint_dir = Gem::Specification.find_by_name("breakpoint").gem_dir
-Sprockets.append_path File.join(breakpoint_dir, "stylesheets")
-
-animation_dir = Gem::Specification.find_by_name("animation").gem_dir
-Sprockets.append_path File.join(animation_dir, "stylesheets")
+#require "compass"
+#require "sprockets"
+#require "jekyll-assets"
+#require "jekyll-assets/compass"
+#
+#susy_dir = Gem::Specification.find_by_name("susy").gem_dir
+#Sprockets.append_path File.join(susy_dir, "sass")
+#
+#breakpoint_dir = Gem::Specification.find_by_name("breakpoint").gem_dir
+#Sprockets.append_path File.join(breakpoint_dir, "stylesheets")
+#
+##respondto_dir = Gem::Specification.find_by_name("respond-to").gem_dir
+##Sprockets.append_path File.join(respondto_dir, "stylesheets")
+#
+#animation_dir = Gem::Specification.find_by_name("animation").gem_dir
+#Sprockets.append_path File.join(animation_dir, "stylesheets")
