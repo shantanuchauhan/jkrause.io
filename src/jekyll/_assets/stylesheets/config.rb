68d5aed3c90578803c0f010d5f152d09914db5d4
Jens Krause <info@websector.de>
2013-01-11 19:40:10 +0100

    stylesheets updated to find the jekyll-assets bug (mentioned before)

---
+++ src/jekyll/_assets/stylesheets/config.rb
@@ -0,0 +1,20 @@
+
+require 'susy'
+require 'animation'
+require 'respond-to'
+
+http_path = '/'
+
+sass_dir = './'
+css_dir = '../../assets/stylesheets'
+http_stylesheets_path = http_path + 'assets/stylesheets'
+
+images_dir = '../../assets/images/'
+http_images_path = http_path + 'assets/images'
+
+fonts_dir = '../../assets/fonts/'
+http_fonts_dir = http_path + 'assets/fonts/'
+
+relative_assets = false
+preferred_syntax = :scss
+disable_warnings = true
